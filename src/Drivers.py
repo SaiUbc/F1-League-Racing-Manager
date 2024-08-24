bc383ed36a010506d3acb2b3f13839a9d83f28d2
SaiUbc <psilegends@gmail.com>
2024-08-25 02:31:31 +0530

    docs: added comment descriptions to all the classes

---
+++ src/Drivers.py
@@ -0,0 +1,10 @@
+"""
+    Represents a driver participating in the F1 League.
+
+    Attributes:
+        name (str): The name of the driver.
+        team (Team): The team that the driver belongs to.
+        points (int): The points the driver has accumulated.
+        position (int): The finishing position of the driver in a race.
+        fastest_lap (bool): Whether the driver has set the
+"""
---
+++ src/Races.py
@@ -0,0 +1,10 @@
+"""
+    Represents a single race in the F1 League.
+
+    Attributes:
+        results (list of Driver): The list of drivers and their finishing positions.
+        penalties (dict of Driver to int): A mapping of drivers to the number of positions they lost due to penalties.
+        fastest_lap (Driver): The driver who set the fastest
+        laps (int): The number of laps
+        weather (str): The weather conditions
+"""
