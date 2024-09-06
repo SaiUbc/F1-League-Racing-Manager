2b9b43b520a0e4838c16d1b0635ba6e80090b77f
SaiUbc <psilegends@gmail.com>
2024-09-06 06:02:31 -0700

    feat: added championship class

--- src/Championship.py
+++ src/Championship.py
@@ -5,3 +5,22 @@
         drivers (list of Driver): The list of all drivers in the championship.
         teams (list of Team): The list of all teams in the championship.
 """
+
+from Drivers import Driver
+
+class Championship:
+    def __init__(self):
+        self.drivers = []
+        self.teams = []
+
+    def add_driver(self, driver):
+        self.drivers.append(driver)
+
+    def add_team(self, team):
+        self.teams.append(team)
+
+    def get_driver_standings(self):
+        return sorted(self.drivers, key=lambda driver: driver.points, reverse=True)
+
+    def get_team_standings(self):
+        return sorted(self.teams, key=lambda team: team.points, reverse=True)
