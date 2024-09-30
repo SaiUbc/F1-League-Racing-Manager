ad9364ad6cfd8c82adfaf378a6c3e792f262d762
SaiUbc <psilegends@gmail.com>
2024-09-30 01:22:09 -0700

    fix: implemented reverse list

--- src/RommateChampionshipF1.py
+++ src/RommateChampionshipF1.py
@@ -2,7 +2,7 @@ import random
 import pandas as pd
 import os
 
-Drivers = ['Rishabh', 'Sai']
+Drivers = ['Arya', 'Rishabh', 'Sai']
 
 Tracks = {
     'australia': "Melbourne Grand Prix Circuit, Australia",
@@ -36,11 +36,11 @@ Tracks = {
 
 Cars_2023 = {
     'mercedes': {'Name': "Mercedes", 'Weight': 796, 'Drag': 0.77674999, 'Aero': 0.0788, 'Aero Front': 0.0461, 'Aero Rear': 0.0327, 'Weight Distro': 0.250},
+    'ferrari': {'Name': "Ferrari", 'Weight': 796, 'Drag': 0.775, 'Aero': 0.0796, 'Aero Front': 0.0457, 'Aero Rear': 0.0339, 'Weight Distro': 0.264},
     'red_bull_racing': {'Name': "Red Bull Racing", 'Weight': 796, 'Drag': 0.76865025, 'Aero': 0.0849, 'Aero Front': 0.0492, 'Aero Rear': 0.0357, 'Weight Distro': 0.264},
     'mclaren': {'Name': "McLaren", 'Weight': 800, 'Drag': 0.775, 'Aero': 0.0808, 'Aero Front': 0.0465, 'Aero Rear': 0.0342, 'Weight Distro': 0.2405},
     'aston_martin': {'Name': "Aston Martin", 'Weight': 805, 'Drag': 0.773249986, 'Aero': 0.0803, 'Aero Front': 0.0463, 'Aero Rear': 0.034, 'Weight Distro': 0.2305},
     'alpine': {'Name': "Alpine", 'Weight': 802, 'Drag': 0.7785, 'Aero': 0.0772, 'Aero Front': 0.0445, 'Aero Rear': 0.0327, 'Weight Distro': 0.236},
-    'ferrari': {'Name': "Ferrari", 'Weight': 796, 'Drag': 0.775, 'Aero': 0.0796, 'Aero Front': 0.0457, 'Aero Rear': 0.0339, 'Weight Distro': 0.264},
     'alphatauri': {'Name': "AlphaTauri", 'Weight': 805, 'Drag': 0.78875033, 'Aero': 0.0716, 'Aero Front': 0.041, 'Aero Rear': 0.0306, 'Weight Distro': 0.2405},
     'alfa_romeo': {'Name': "Alfa Romeo", 'Weight': 803, 'Drag': 0.784749997, 'Aero': 0.0727, 'Aero Front': 0.042, 'Aero Rear': 0.0309, 'Weight Distro': 0.264},
     'haas': {'Name': "Haas", 'Weight': 807, 'Drag': 0.778, 'Aero': 0.0763, 'Aero Front': 0.0439, 'Aero Rear': 0.0324, 'Weight Distro': 0.245},
@@ -80,8 +80,8 @@ def record_race_results(Drivers, selected_track, car):
 
 
 def main():
-    track = 'singapore'
-    for car in Cars_2023:
+    track = 'australia'
+    for car in reversed(Cars_2023):
         record_race_results(Drivers, track, Cars_2023[car]['Name'])
                 
 if __name__ == "__main__":
