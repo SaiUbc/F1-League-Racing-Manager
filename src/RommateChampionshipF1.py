6ff419218e8c965777b1db0149d8e9359526b403
SaiUbc <psilegends@gmail.com>
2024-09-29 17:22:46 -0700

    feat: updated F1 2023 car performance parameters

--- src/RommateChampionshipF1.py
+++ src/RommateChampionshipF1.py
@@ -16,7 +16,6 @@ Tracks = {
     'netherlands': "Circuit Zandvoort, Netherlands",
     'spain': "Circuit de Barcelona-Catalunya, Spain",
     'monaco': "Circuit de Monaco, Monaco",
-    'azerbaijan': "Baku City Circuit, Azerbaijan",
     'canada': "Circuit Gilles Villeneuve, Canada",
     'france': "Circuit Paul Ricard, France",
     'austria': "Red Bull Ring, Austria",
@@ -35,17 +34,17 @@ Tracks = {
 }
 
 
-Cars = {
-    'mercedes': {'Name': "Mercedes", 'Drivetrain': 'MR', 'Power': 950, 'Weight': 746},
-    'red_bull_racing': {'Name': "Red Bull Racing", 'Drivetrain': 'MR', 'Power': 950, 'Weight': 746},
-    'mclaren': {'Name': "McLaren", 'Drivetrain': 'MR', 'Power': 950, 'Weight': 746},
-    'aston_martin': {'Name': "Aston Martin", 'Drivetrain': 'MR', 'Power': 950, 'Weight': 746},
-    'alpine': {'Name': "Alpine", 'Drivetrain': 'MR', 'Power': 950, 'Weight': 746},
-    'ferrari': {'Name': "Ferrari", 'Drivetrain': 'MR', 'Power': 950, 'Weight': 746},
-    'alphatauri': {'Name': "AlphaTauri", 'Drivetrain': 'MR', 'Power': 950, 'Weight': 746},
-    'alfa_romeo': {'Name': "Alfa Romeo", 'Drivetrain': 'MR', 'Power': 950, 'Weight': 746},
-    'haas': {'Name': "Haas", 'Drivetrain': 'MR', 'Power': 950, 'Weight': 746},
-    'williams': {'Name': "Williams", 'Drivetrain': 'MR', 'Power': 950, 'Weight': 746}
+Cars_2023 = {
+    'mercedes': {'Name': "Mercedes", 'Weight': 796, 'Drag': 0.77674999, 'Aero': 0.0788, 'Aero Front': 0.0461, 'Aero Rear': 0.0327, 'Weight Distro': 0.250},
+    'red_bull_racing': {'Name': "Red Bull Racing", 'Weight': 796, 'Drag': 0.76865025, 'Aero': 0.0849, 'Aero Front': 0.0492, 'Aero Rear': 0.0357, 'Weight Distro': 0.264},
+    'mclaren': {'Name': "McLaren", 'Weight': 800, 'Drag': 0.775, 'Aero': 0.0808, 'Aero Front': 0.0465, 'Aero Rear': 0.0342, 'Weight Distro': 0.2405},
+    'aston_martin': {'Name': "Aston Martin", 'Weight': 805, 'Drag': 0.773249986, 'Aero': 0.0803, 'Aero Front': 0.0463, 'Aero Rear': 0.034, 'Weight Distro': 0.2305},
+    'alpine': {'Name': "Alpine", 'Weight': 802, 'Drag': 0.7785, 'Aero': 0.0772, 'Aero Front': 0.0445, 'Aero Rear': 0.0327, 'Weight Distro': 0.236},
+    'ferrari': {'Name': "Ferrari", 'Weight': 796, 'Drag': 0.775, 'Aero': 0.0796, 'Aero Front': 0.0457, 'Aero Rear': 0.0339, 'Weight Distro': 0.264},
+    'alphatauri': {'Name': "AlphaTauri", 'Weight': 805, 'Drag': 0.78875033, 'Aero': 0.0716, 'Aero Front': 0.041, 'Aero Rear': 0.0306, 'Weight Distro': 0.2405},
+    'alfa_romeo': {'Name': "Alfa Romeo", 'Weight': 803, 'Drag': 0.784749997, 'Aero': 0.0727, 'Aero Front': 0.042, 'Aero Rear': 0.0309, 'Weight Distro': 0.264},
+    'haas': {'Name': "Haas", 'Weight': 807, 'Drag': 0.778, 'Aero': 0.0763, 'Aero Front': 0.0439, 'Aero Rear': 0.0324, 'Weight Distro': 0.245},
+    'williams': {'Name': "Williams", 'Weight': 809, 'Drag': 0.7709999, 'Aero': 0.0749, 'Aero Front': 0.0428, 'Aero Rear': 0.0321, 'Weight Distro': 0.235}
 }
 
 
@@ -81,9 +80,9 @@ def record_race_results(Drivers, selected_track, car):
 
 
 def main():
-    track = 'japan'
-    for car in Cars:
-        record_race_results(Drivers, track, Cars[car]['Name'])
+    track = 'singapore'
+    for car in Cars_2023:
+        record_race_results(Drivers, track, Cars_2023[car]['Name'])
                 
 if __name__ == "__main__":
     main()
